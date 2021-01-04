b972b3cbff9fd9ab5341a4e58018d4b1e8133a57
zeniraltis <deadmandieddeathless@gmail.com>
2021-01-05 00:25:08 +0530

    got rid of useless things. got closer to creating python interpreter inside discord.

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import discord
+import os
 from discord.ext import commands
+from subprocess import run
+
+with open('token.txt', 'r') as f:
+	TOKEN = str(f.readline())
 
 client = commands.Bot(command_prefix = 'a.')
 
@@ -17,58 +22,20 @@ async def kilu(ctx):
 	await ctx.send('Killerok, do your assignment.')
 
 @client.command()
-async def addmem(ctx, name: discord.Member, lang="Python"):
+async def code(ctx, *, args):
+	await ctx.send(args)
+
+@client.command()
+async def run(ctx, *, args):
+	with open("tester.py", "w") as f:
+		f.write(args)
 	try:
-		with open("data.json", "r") as f:
-			file = json.load(f)
+		os.system('python tester.py')
 
-		with open("names.json", "r") as f:
-			data = json.load(f)	
-
-		if name not in file:	
-
-			file[str(name.id)] = lang
-			data[str(name.id)] = name
-
-			with open("data.json", "w") as f:
-				json.dump(file, f)
-			with open("names.json", "w") as f:
-				json.dump(data, f)	
-
-			await ctx.send('User has been successfuly added.')	
-
-		elif name in file:
-			await ctx.send('User is already in database.')
-		else:
-			raise Exception("smth")
-	except Exception as e:
-		await ctx.send(e)
-
-@client.command()
-async def removemem(ctx, name):
-		with open("data.json", "r") as f:
-			file = json.load(f)
-
-		if name in file:
-			del file[str(name)]
-			await ctx.send('User has been successfuly removed.')
-		elif name not in file:
-			await ctx.send("User not found.")
-		else:
-			raise Exception("smth else")
-
-@client.command()
-async def viewmem(ctx):
-		with open("names.json", "r") as f:
-			file = json.load(f)
-		mem = ""
-		c = 0
-		
-		for i in file:
-			c += 1
-			mem.join(f"[{c}] {i}\n")
+	except Exception as e:		
+		await ctx.send(f"WARNING:\n{e}")
 
 	
 #Run		
 
-client.run('Nzk1NTA4ODUwMDM5ODQ4OTgw.X_KZWQ.KvEAP-Qe0BqV8rux5qAJd2FFPoM')	
+client.run(TOKEN)	
---
+++ tester.py
@@ -0,0 +1,4 @@
+def test():
+	def xyz():
+		pass
+	return xyz()
